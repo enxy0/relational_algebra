15116a537e65f4caeff0c40da45416fda6364fab
enxy0 <enxyox@gmail.com>
2020-04-24 02:54:57 +0300

    Add cartesianProduct algorithm Добавлен алгоритм создания декартова произведения из двух Collection

--- src/mingwMain/kotlin/Utils.kt
+++ src/mingwMain/kotlin/Utils.kt
@@ -15,3 +15,16 @@ infix fun Boolean.differs(other: Boolean) = this and !other
 
 // Symmetric Difference (∆)
 infix fun Boolean.symDiffers(other: Boolean) = (this and !other) or (!this and other)
+
+/**
+ * Декартово произведение двух [Collection]
+ * in: cartesianProduct(listOf(1, 2, 3), listOf(1, 2, 3))
+ * out: [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
+ * @param c1 Первая [Collection]
+ * @param c2 Вторая [Collection]
+ * @return [List] из [Pair], где каждый элемент первой [Collection] сопоставлен
+ * со всеми элементами второй [Collection]
+ */
+fun <T, U> cartesianProduct(c1: Collection<T>, c2: Collection<U>): List<Pair<T, U>> {
+    return c1.flatMap { first -> c2.map { second -> first to second } }
+}
